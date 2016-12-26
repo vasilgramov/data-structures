17549ef170902ba7630592215cc279b223b94f0f
Vladix97 <gramovv@gmail.com>
2016-12-26 16:07:46 +0200

    extract max method

--- 07.AdvancedTreeStructures02/Exercises/src/p01_binaryHeap/BinaryHeap.java
+++ 07.AdvancedTreeStructures02/Exercises/src/p01_binaryHeap/BinaryHeap.java
@@ -1,7 +1,17 @@
 package p01_binaryHeap;
 
+import sun.plugin.dom.exception.InvalidAccessException;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+
+/**
+ *
+ * this is a max binary heap
+ *
+ * down below i've added comments which will
+ * help you to transfer it to min heap
+ */
 
 public class BinaryHeap<T extends Comparable<T>> {
     private ArrayList<T> heap;
@@ -17,10 +27,36 @@ public class BinaryHeap<T extends Comparable<T>> {
         }
     }
 
+    public int count() {
+        return this.heap.size();
+    }
+
     public void add(T element) {
         this.heap.add(element);
 
         heapifyUp(this.heap.size() - 1);
+    }
+
+    public T extractMax() {
+        if (this.heap.size() == 0) {
+            throw new InvalidAccessException("Heap is empty!");
+        }
+
+        T toReturn = this.heap.get(0);
+
+        this.heap.set(0, this.heap.get(this.heap.size() - 1));
+        this.heap.remove(this.heap.size() - 1);
+        heapifyDown(0);
+
+        return toReturn;
+    }
+
+    public T peek() {
+        if (this.heap.size() == 0) {
+            throw new InvalidAccessException("Heap is empty!");
+        }
+
+        return this.heap.get(0);
     }
 
     private void heapifyDown(int elementIndex) {
@@ -28,11 +64,11 @@ public class BinaryHeap<T extends Comparable<T>> {
         int rightChildIndex = elementIndex * 2 + 2;
         int largest = elementIndex;
 
-        if (this.heap.size() > leftChildIndex && this.heap.get(leftChildIndex).compareTo(this.heap.get(largest)) > 0) {
+        if (this.heap.size() > leftChildIndex && this.heap.get(leftChildIndex).compareTo(this.heap.get(largest)) > 0) {     // change the last operator to  "<"
             largest = leftChildIndex;
         }
 
-        if (this.heap.size() > rightChildIndex && this.heap.get(rightChildIndex).compareTo(this.heap.get(largest)) > 0) {
+        if (this.heap.size() > rightChildIndex && this.heap.get(rightChildIndex).compareTo(this.heap.get(largest)) > 0) {   // change the last operator to  "<"
             largest = rightChildIndex;
         }
 
@@ -48,7 +84,7 @@ public class BinaryHeap<T extends Comparable<T>> {
     private void heapifyUp(int elementIndex) {
         int parentIndex = (elementIndex - 1) / 2;
 
-        if (this.heap.get(parentIndex).compareTo(this.heap.get(elementIndex)) < 0) {
+        if (this.heap.get(parentIndex).compareTo(this.heap.get(elementIndex)) < 0) {    // change operator to  ">"
             T old = this.heap.get(elementIndex);
             this.heap.set(elementIndex, this.heap.get(parentIndex));
             this.heap.set(parentIndex, old);
--- 07.AdvancedTreeStructures02/Exercises/src/p01_binaryHeap/Startup.java
+++ 07.AdvancedTreeStructures02/Exercises/src/p01_binaryHeap/Startup.java
@@ -3,12 +3,14 @@ package p01_binaryHeap;
 public class Startup {
     public static void main(String[] args) {
 
-        Integer[] numbers = new Integer[] { 3, 7, 1, 5, 9, 8 };
+        Integer[] numbers = new Integer[] { 17, 3, 19, 36, 25, 1, 2, 7, 100 };
 
-        BinaryHeap<Integer> binaryHeap = new BinaryHeap<Integer>();
-        binaryHeap.peek();
+        BinaryHeap<Integer> binaryHeap = new BinaryHeap<Integer>(numbers);
+        binaryHeap.add(123);
 
-        binaryHeap.add(111);
-        System.out.println("");
+        while (binaryHeap.count() > 0) {
+            System.out.println(binaryHeap.peek());
+            binaryHeap.extractMax();
+        }
     }
 }
